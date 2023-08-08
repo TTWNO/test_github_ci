0b213eee7a2401aa49c5d7e901cc321a4d79be35
Tait Hoyem <tait@tait.tech>
2023-08-08 15:16:31 -0600

    initial commmit

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+#![deny(
+  clippy::all,
+  //clippy::pedantic,
+  //unsafe_code,
+  //missing_docs,
+)]
+
+// //! My library is to do simple math
+
+// /// We need documentation to make `missing_docs` work
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+// TODO: fix clippy error
+// local (not public) function which is not used
+//fn mult(left: usize, right: usize) -> usize {
+//  left * right
+//}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    #[should_panic]
+    fn lol() {
+      panic!("Oh no!");
+    }
+}
